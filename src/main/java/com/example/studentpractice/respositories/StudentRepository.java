f4021dfeab0f86c607a0bae3820b09afe27baef6
alanlhy94 <115515042+alanlhy94@users.noreply.github.com>
2022-10-18 20:18:55 -0700

    Initial commit

---
+++ src/main/java/com/example/studentpractice/respositories/StudentRepository.java
@@ -0,0 +1,5 @@
+package com.example.studentpractice.respositories;
+
+public interface StudentRepository extends JpaRepository<Student,Long> {
+}
+
---
+++ src/test/java/com/example/studentpractice/StudentPracticeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.studentpractice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudentPracticeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
